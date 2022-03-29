ca7b1cb9582412afe3bae1fdb8465abf9e2a427c
Nick Smith <nick@astral.blog>
2022-03-29 17:21:48 +1100

    Snap to lines using projection (first try)

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,40 @@
 <script lang="ts">
    import { Vec, Line } from "./math"
-   // Angle constants
-   const tau = 2 * Math.PI
+   // Snapping
    const snapDistanceSq = 15 * 15
    const lengthSqAtWhichSnapsActivate = 2 * snapDistanceSq
+
+   // Snap if applicable, and return the outcome.
+   function trySnapToPoint(source: Vec, target: Vec): [boolean, Vec] {
+      if (source.distanceSq(target) < snapDistanceSq) {
+         return [true, target.clone()]
+      } else {
+         return [false, source]
+      }
+   }
+   function trySnapToLine(source: Vec, target: Line): [boolean, Vec] {
+      // Use coordinates relative to one endpoint of the line
+      let s = source.sub(target.start)
+      let t = target.end.sub(target.start)
+      let lenSq = t.lengthSq()
+      let dot = s.dot(t)
+      // The source is "next to" the line iff the dot product is in [0, lenSq]
+      if (dot >= 0 && dot <= lenSq) {
+         let projection = t.scale(dot / lenSq)
+         let rejection = s.sub(projection)
+         if (rejection.lengthSq() < snapDistanceSq) {
+            return [true, target.start.add(projection)]
+         } else {
+            return [false, source]
+         }
+      } else {
+         return [false, source]
+      }
+   }
+
+   // Angle constants
+   const tau = 2 * Math.PI
+
    const snapAngles = [
       // Ordered by priority
       0,
@@ -18,47 +49,48 @@
    const snapVectors = snapAngles.map((a) => {
       return new Vec(Math.cos(a), Math.sin(a))
    })
-   // State
+   // --------------- State ---------------
    let mouse: Vec = new Vec(0, 0)
    let lines: Line[] = []
+   // The line that defines the current metric used for drawing
+   let referenceLine: { line: Line; length: number } | null = null
+   // Drawing state
    let lineStart: Vec | null = null
    let lineEnd: Vec | null
 
    $: {
       if (lineStart) {
-         let snapped = false
-         // If the line is close to the endpoint of another line, then snap it.
+         // By default, the line endpoint should be at the mouse position.
+         lineEnd = mouse.clone()
+         // But we're going to try and snap it to a bunch of things.
+         let snappedToPoint = false
+         // Priority #1: Try snapping to line endpoints.
          for (let line of lines) {
-            if (mouse.distanceSq(line.start) < snapDistanceSq) {
-               lineEnd = line.start.clone()
-               snapped = true
-               break
-            }
-            if (mouse.distanceSq(line.end) < snapDistanceSq) {
-               lineEnd = line.end.clone()
-               snapped = true
-               break
-            }
+            ;[snappedToPoint, lineEnd] = trySnapToPoint(lineEnd, line.start)
+            if (snappedToPoint) break
+            ;[snappedToPoint, lineEnd] = trySnapToPoint(lineEnd, line.end)
+            if (snappedToPoint) break
          }
-         // If the line is close to a snap angle, then snap it.
-         // TODO: If we are extending an existing line, we should also snap to
-         // that line's angle, and to orthogonal angles.
-         const dv = mouse.sub(lineStart)
-         if (!snapped && dv.lengthSq() >= lengthSqAtWhichSnapsActivate) {
+         // Priority #2: Try snapping to angles.
+         const dv = lineEnd.sub(lineStart)
+         let snapped = false
+         if (!snappedToPoint && dv.lengthSq() >= lengthSqAtWhichSnapsActivate) {
             for (let dir of snapVectors) {
                // Projection onto unit vector
                const p = dir.scale(dv.dot(dir))
-               // Rejection from unit vector
-               const d = dv.sub(p)
-               if (d.lengthSq() < snapDistanceSq) {
+               ;[snapped] = trySnapToPoint(dv, p)
+               if (snapped) {
                   lineEnd = lineStart.add(p)
-                  snapped = true
                   break
                }
             }
          }
-         if (!snapped) {
-            lineEnd = mouse.clone()
+         // Priority #3: Try snapping to lines.
+         if (!snappedToPoint) {
+            for (let line of lines) {
+               ;[snapped, lineEnd] = trySnapToLine(lineEnd, line)
+               if (snapped) break
+            }
          }
       } else {
          lineEnd = null
@@ -73,19 +105,13 @@
    on:pointerdown={(event) => {
       let click = new Vec(event.clientX, event.clientY)
       // If we clicked near the endpoint of a line, snap to it.
-      // TODO: This code is a duplicate of the lineEnd code. Refactor it.
       let snapped = false
       for (let line of lines) {
-         if (click.distanceSq(line.start) < snapDistanceSq) {
-            lineStart = line.start.clone()
-            snapped = true
-            break
-         }
-         if (click.distanceSq(line.end) < snapDistanceSq) {
-            lineStart = line.end.clone()
-            snapped = true
-            break
-         }
+         ;[snapped, lineStart] = trySnapToPoint(click, line.start)
+         if (snapped) break
+         ;[snapped, lineStart] = trySnapToPoint(click, line.end)
+         if (snapped) break
+         trySnapToLine(click, line)
       }
       if (!snapped) {
          lineStart = click
@@ -93,18 +119,18 @@
    }}
    on:pointerup={() => {
       if (lineStart && lineEnd) {
-         lines = [...lines, { start: lineStart, end: lineEnd }]
+         lines = [...lines, new Line(lineStart, lineEnd)]
          lineStart = null
       }
    }}
 >
    {#each lines as l}
       <line x1={l.start.x} y1={l.start.y} x2={l.end.x} y2={l.end.y} />
+      {#each [...l.points(3)] as p}
+         <circle cx={p.x} cy={p.y} r={4} />
+      {/each}
    {/each}
    {#if lineStart && lineEnd}
-      <text x="16px" y="40px">
-         The end position is {lineEnd.x} x {lineEnd.y}
-      </text>
       <line x1={lineStart.x} y1={lineStart.y} x2={lineEnd.x} y2={lineEnd.y} />
    {/if}
 </svg>
@@ -122,5 +148,8 @@
    line {
       stroke: rgb(106, 2, 167);
       stroke-width: 2px;
+   }
+   circle {
+      fill: black;
    }
 </style>
--- src/math.ts
+++ src/math.ts
@@ -1,5 +1,3 @@
-export type Line = { start: Vec; end: Vec }
-
 export class Vec {
    x: number
    y: number
@@ -31,7 +29,37 @@ export class Vec {
    dot(v: Vec): number {
       return this.x * v.x + this.y * v.y
    }
+   copy(v: Vec) {
+      this.x = v.x
+      this.y = v.y
+   }
    clone(): Vec {
       return new Vec(this.x, this.y)
+   }
+}
+
+export class Line {
+   start: Vec
+   end: Vec
+   constructor(start: Vec, end: Vec) {
+      this.start = start
+      this.end = end
+   }
+
+   // Returns the endpoints of the line, plus the specified number of
+   // interior points (equally spaced).
+   *points(interiorPoints: number) {
+      yield this.start
+      if (interiorPoints > 0) {
+         let d = this.end.sub(this.start).scale(1 / (interiorPoints + 1))
+         for (
+            let i = 0, p = this.start.add(d);
+            i < interiorPoints;
+            ++i, p = p.add(d)
+         ) {
+            yield p
+         }
+      }
+      yield this.end
    }
 }
