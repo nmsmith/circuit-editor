eed1ab5758c041501022871d9de7821493c79c92
Nick Smith <nick@astral.blog>
2022-03-30 15:59:17 +1100

    Added logic for snapping to arbitrary points along line segments

--- src/App.svelte
+++ src/App.svelte
@@ -1,43 +1,13 @@
 <script lang="ts">
-   import { Vec, Point, Line } from "./math"
+   import { Vec, Point, Ray, Segment } from "./math"
+   // Angle constants
+   const tau = 2 * Math.PI
    // Snapping
    const sqSnapDistance = 15 * 15
    const sqLengthAtWhichSnapsActivate = 2 * sqSnapDistance
-
-   // Snap if applicable, and return the outcome.
-   function trySnapToPoint(source: Point, target: Point): [boolean, Point] {
-      if (source.sqDistanceFrom(target) < sqSnapDistance) {
-         return [true, target.clone()]
-      } else {
-         return [false, source]
-      }
-   }
-   // If close to the target line, snap by "growing"
-   function trySnapToLine(source: Point, target: Line): [boolean, Point] {
-      // Use coordinates relative to one endpoint of the line
-      let s = source.displacementFrom(target.start)
-      let t = target.end.displacementFrom(target.start)
-      let lenSq = t.sqLength()
-      let dot = s.dot(t)
-      // The source is "next to" the line iff the dot product is in [0, lenSq]
-      if (dot >= 0 && dot <= lenSq) {
-         let projection = t.scaleBy(dot / lenSq)
-         let rejection = s.sub(projection)
-         if (rejection.sqLength() < sqSnapDistance) {
-            return [true, target.start.displaceBy(projection)]
-         } else {
-            return [false, source]
-         }
-      } else {
-         return [false, source]
-      }
-   }
-
-   // Angle constants
-   const tau = 2 * Math.PI
-
+   const cosMinimumSnapAngle = Math.cos(tau / 25) // When snapping to a segment
    const snapAngles = [
-      // Ordered by priority
+      // Angles used when snapping to an angle (ordered by priority)
       0,
       0.25 * tau,
       0.5 * tau,
@@ -50,54 +20,118 @@
    const snapVectors = snapAngles.map((a) => {
       return new Vec(Math.cos(a), Math.sin(a))
    })
+
+   function pointsCanSnap(p: Point, q: Point): boolean {
+      return p.sqDistanceFrom(q) < sqSnapDistance
+   }
+   function trySnapToSegment(
+      point: Point,
+      target: Segment,
+      snapDirection?: Vec // If empty, we will just project onto the segment.
+   ): Point | null {
+      // Use coordinates relative to one endpoint of the segment.
+      let p = point.displacementFrom(target.start)
+      let t = target.end.displacementFrom(target.start)
+      let sqLength = t.sqLength()
+      let dot = p.dot(t)
+      // Only attempt to snap the point if its projection lies on the segment.
+      // This occurs iff the dot product is in [0, lenSq].
+      if (dot >= 0 && dot <= sqLength) {
+         // Only snap if the point is sufficiently close to the segment.
+         let projection = t.scaleBy(dot / sqLength)
+         let rejection = p.sub(projection)
+         if (rejection.sqLength() < sqSnapDistance) {
+            // If we have been given a snap direction, and the angle between
+            // the segment and the snap direction is not too narrow, attempt
+            // to snap to that direction.
+            if (snapDirection) {
+               let cosAngle =
+                  t.dot(snapDirection) / (t.length() * snapDirection.length())
+               if (Math.abs(cosAngle) <= cosMinimumSnapAngle) {
+                  let forward = new Ray(point, snapDirection)
+                  let backward = new Ray(point, snapDirection.scaleBy(-1))
+                  return (
+                     forward.intersection(target) ||
+                     backward.intersection(target)
+                  )
+               } else return null
+            } else {
+               // Otherwise, snap in the direction of the projection.
+               return target.start.displaceBy(projection)
+            }
+         } else return null
+      } else return null
+   }
+
    // --------------- State ---------------
    let mouse: Point = new Point(0, 0)
-   let lines: Line[] = []
-   // The line that defines the current metric used for drawing
-   let referenceLine: { line: Line; length: number } | null = null
+   let segments: Segment[] = []
+   function* endpoints() {
+      for (let seg of segments) {
+         yield seg.start
+         yield seg.end
+      }
+   }
+   let segmentUnderMouse: Segment | null
+   // The segment that defines the current metric used for drawing
+   let reference: { segment: Segment; length: number } | null = null
    // Drawing state
-   let lineStart: Point | null = null
-   let lineEnd: Point | null
+   let segStart: Point | null = null
+   let segEnd: Point | null
 
    $: {
-      if (lineStart) {
-         // By default, the line endpoint should be at the mouse position.
-         lineEnd = mouse.clone()
-         // But we're going to try and snap it to a bunch of things.
-         let snappedToPoint = false
-         // Priority #1: Try snapping to line endpoints.
-         for (let line of lines) {
-            ;[snappedToPoint, lineEnd] = trySnapToPoint(lineEnd, line.start)
-            if (snappedToPoint) break
-            ;[snappedToPoint, lineEnd] = trySnapToPoint(lineEnd, line.end)
-            if (snappedToPoint) break
-         }
-         // Priority #2: Try snapping to angles.
-         const dirLine = lineEnd.displacementFrom(lineStart)
-         let snapped = false
-         if (
-            !snappedToPoint &&
-            dirLine.sqLength() >= sqLengthAtWhichSnapsActivate
-         ) {
-            for (let dirSnap of snapVectors) {
-               const dirProjected = dirLine.projectionOnto(dirSnap)
-               const rejection = dirLine.sub(dirProjected)
-               if (rejection.sqLength() < sqSnapDistance) {
-                  snapped = true
-                  lineEnd = lineStart.displaceBy(dirProjected)
+      if (segStart) {
+         // By default, the segment endpoint should be at the mouse position.
+         segEnd = mouse.clone()
+         // Priority #1: Try snapping to points along the reference segment.
+         let snappedToPoint
+         if (reference) {
+            for (let p of reference.segment.points(reference.length - 1)) {
+               if (pointsCanSnap(segEnd, p)) {
+                  segEnd = snappedToPoint = p.clone()
                   break
                }
             }
          }
-         // Priority #3: Try snapping to lines.
+         // Priority #2: Try snapping to segment endpoints.
          if (!snappedToPoint) {
-            for (let line of lines) {
-               ;[snapped, lineEnd] = trySnapToLine(lineEnd, line)
-               if (snapped) break
+            for (let p of endpoints()) {
+               if (pointsCanSnap(segEnd, p)) {
+                  segEnd = snappedToPoint = p.clone()
+                  break
+               }
+            }
+         }
+         // Priority #3: Try snapping to angles.
+         const dir = segEnd.displacementFrom(segStart)
+         let snapped = false
+         if (
+            !snappedToPoint &&
+            dir.sqLength() >= sqLengthAtWhichSnapsActivate
+         ) {
+            for (let dirSnap of snapVectors) {
+               const dirProjected = dir.projectionOnto(dirSnap)
+               const rejection = dir.sub(dirProjected)
+               if (rejection.sqLength() < sqSnapDistance) {
+                  snapped = true
+                  segEnd = segStart.displaceBy(dirProjected)
+                  break
+               }
+            }
+         }
+         // Priority #4: Try snapping to segments.
+         if (!snappedToPoint) {
+            let snapDirection = segEnd.displacementFrom(segStart)
+            for (let seg of segments) {
+               snappedToPoint = trySnapToSegment(segEnd, seg, snapDirection)
+               if (snappedToPoint) {
+                  segEnd = snappedToPoint
+                  break
+               }
             }
          }
       } else {
-         lineEnd = null
+         segEnd = null
       }
    }
 </script>
@@ -107,35 +141,39 @@
       mouse = new Point(event.clientX, event.clientY)
    }}
    on:pointerdown={(event) => {
-      let click = new Point(event.clientX, event.clientY)
-      // If we clicked near the endpoint of a line, snap to it.
-      let snapped = false
-      for (let line of lines) {
-         ;[snapped, lineStart] = trySnapToPoint(click, line.start)
-         if (snapped) break
-         ;[snapped, lineStart] = trySnapToPoint(click, line.end)
-         if (snapped) break
-         trySnapToLine(click, line)
-      }
-      if (!snapped) {
-         lineStart = click
+      segStart = new Point(event.clientX, event.clientY)
+      // If we clicked near the endpoint of a segment, snap to it.
+      for (let p of endpoints()) {
+         if (pointsCanSnap(segStart, p)) {
+            segStart = p.clone()
+            break
+         }
       }
    }}
    on:pointerup={() => {
-      if (lineStart && lineEnd) {
-         lines = [...lines, new Line(lineStart, lineEnd)]
-         lineStart = null
+      if (segStart && segEnd) {
+         segments = [...segments, new Segment(segStart, segEnd)]
+         segStart = null
       }
    }}
 >
-   {#each lines as l}
-      <line x1={l.start.x} y1={l.start.y} x2={l.end.x} y2={l.end.y} />
-      {#each [...l.points(3)] as p}
+   <!-- {#if segStart && segEnd}
+      {#each segments as seg}
+         <circle
+            r={7}
+            cx={new Segment(segStart, segEnd).intersection(seg)?.x}
+            cy={new Segment(segStart, segEnd).intersection(seg)?.y}
+         />
+      {/each}
+   {/if} -->
+   {#each segments as seg}
+      <line x1={seg.start.x} y1={seg.start.y} x2={seg.end.x} y2={seg.end.y} />
+      {#each [...seg.points(0)] as p}
          <circle cx={p.x} cy={p.y} r={4} />
       {/each}
    {/each}
-   {#if lineStart && lineEnd}
-      <line x1={lineStart.x} y1={lineStart.y} x2={lineEnd.x} y2={lineEnd.y} />
+   {#if segStart && segEnd}
+      <line x1={segStart.x} y1={segStart.y} x2={segEnd.x} y2={segEnd.y} />
    {/if}
 </svg>
 
