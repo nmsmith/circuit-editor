e2f8998244d0ed7917f4a1ac3cefd6bdd4736c96
Nick Smith <nick@astral.blog>
2022-04-04 15:34:29 +1000

    Refactor snapping code
    Removed all code duplication.

--- src/App.svelte
+++ src/App.svelte
@@ -21,116 +21,115 @@
       return new Vec(Math.cos(a), Math.sin(a))
    })
 
-   function pointsCanSnap(p: Point, q: Point): boolean {
-      return p.sqDistanceFrom(q) < sqSnapDistance
+   // Snap the point to a significant point in the scene, if possible.
+   // Otherwise, return the same point.
+   function trySnappingToPoint(point: Point): Point {
+      // Try snapping to segment endpoints.
+      for (let p of circuitUndir.keys()) {
+         if (p.sqDistanceFrom(point) < sqSnapDistance) return p
+      }
+      // Try snapping to points along the reference segment.
+      if (reference) {
+         for (let p of reference.segment.points(reference.length - 1)) {
+            if (p.sqDistanceFrom(point) < sqSnapDistance) return p
+         }
+      }
+      return point
    }
-   function trySnapToSegment(
+   // Snap the point to a nearby line segment, if possible.
+   // Otherwise, return the same point.
+   function trySnappingToSegment(
       point: Point,
-      target: Segment,
       snapDirection?: Vec // If empty, we will just project onto the segment.
-   ): Point | null {
-      // Use coordinates relative to one endpoint of the segment.
-      let p = point.displacementFrom(target.start)
-      let t = target.end.displacementFrom(target.start)
-      let sqLength = t.sqLength()
-      let dot = p.dot(t)
-      // Only attempt to snap the point if its projection lies on the segment.
-      // This occurs iff the dot product is in [0, lenSq].
-      if (dot >= 0 && dot <= sqLength) {
-         // Only snap if the point is sufficiently close to the segment.
-         let projection = t.scaleBy(dot / sqLength)
-         let rejection = p.sub(projection)
-         if (rejection.sqLength() < sqSnapDistance) {
-            // If we have been given a snap direction, and the angle between
-            // the segment and the snap direction is not too narrow, attempt
-            // to snap to that direction.
-            if (snapDirection) {
-               let cosAngle =
-                  t.dot(snapDirection) / (t.length() * snapDirection.length())
-               if (Math.abs(cosAngle) <= cosMinimumSnapAngle) {
-                  let forward = new Ray(point, snapDirection)
-                  let backward = new Ray(point, snapDirection.scaleBy(-1))
-                  return (
-                     forward.intersection(target) ||
-                     backward.intersection(target)
-                  )
-               } else return null
-            } else {
-               // Otherwise, snap in the direction of the projection.
-               return target.start.displaceBy(projection)
+   ): Point {
+      for (let [start, end] of segments()) {
+         // Use coordinates relative to one endpoint of the segment.
+         let p = point.displacementFrom(start)
+         let t = end.displacementFrom(start)
+         let sqLength = t.sqLength()
+         let dot = p.dot(t)
+         // Only try snapping if the point's projection lies on the segment.
+         // This occurs iff the dot product is in [0, lenSq].
+         if (dot >= 0 && dot <= sqLength) {
+            // Only snap if the point is sufficiently close to the segment.
+            let projection = t.scaleBy(dot / sqLength)
+            let rejection = p.sub(projection)
+            if (rejection.sqLength() < sqSnapDistance) {
+               // If we have been given a snap direction, and the angle
+               // between the segment and the snap direction is not too
+               // narrow, attempt to snap to that direction.
+               if (snapDirection) {
+                  let cosAngle =
+                     t.dot(snapDirection) /
+                     (t.length() * snapDirection.length())
+                  if (Math.abs(cosAngle) <= cosMinimumSnapAngle) {
+                     let forward = new Ray(point, snapDirection)
+                     let backward = new Ray(point, snapDirection.scaleBy(-1))
+                     let target = new Segment(start, end)
+                     let intersection =
+                        forward.intersection(target) ||
+                        backward.intersection(target)
+                     if (intersection) {
+                        return intersection
+                     }
+                  }
+               } else {
+                  // Otherwise, snap in the direction of the projection.
+                  return start.displaceBy(projection)
+               }
             }
-         } else return null
-      } else return null
+         }
+      }
+      return point
    }
 
    // --------------- State ---------------
-   let mouse: Point = new Point(0, 0)
+   // The circuit is stored as an adjacency list to enable efficient graph
+   // traversals. We store the circuit twice. The first encoding is directed,
+   // so every edge is only stored once. This is necessary for some operations.
+   // The second encoding is undirected, so every edge is stored twice. This
+   // is necessary for graph traversal operations.
    let circuitDir: Map<Point, Set<Point>> = new Map()
    let circuitUndir: Map<Point, Set<Point>> = new Map()
+   function* segments() {
+      for (let [start, ends] of circuitDir) {
+         for (let end of ends) {
+            yield [start, end]
+         }
+      }
+   }
    let segmentUnderMouse: Segment | null
    // The segment that defines the current metric used for drawing
    let reference: { segment: Segment; length: number } | null = null
-   // Drawing state
-   let segStart: Point | null = null
-   let segEnd: Point | null
+   // Input state
+   let mouse: Point = Point.zero
+   let drawStart: Point | null = null
+   let drawEnd: Point | null
+   let dragStart: Point | null = null
+   let dragEnd: Point | null
 
    $: {
-      if (segStart) {
-         // By default, the segment endpoint should be at the mouse position.
-         segEnd = mouse
-         let snappedToPoint
-         // Priority #1: Try snapping to points along the reference segment.
-         if (reference) {
-            for (let p of reference.segment.points(reference.length - 1)) {
-               if (pointsCanSnap(segEnd, p)) {
-                  segEnd = snappedToPoint = p
-                  break
-               }
-            }
-         }
-         // Priority #2: Try snapping to segment endpoints.
-         if (!snappedToPoint) {
-            for (let p of circuitUndir.keys()) {
-               if (pointsCanSnap(segEnd, p)) {
-                  segEnd = snappedToPoint = p
-                  break
-               }
-            }
-         }
-         // Priority #3: Try snapping to angles.
-         const dir = segEnd.displacementFrom(segStart)
-         if (
-            !snappedToPoint &&
-            dir.sqLength() >= sqLengthAtWhichSnapsActivate
-         ) {
-            for (let dirSnap of snapVectors) {
-               const dirProjected = dir.projectionOnto(dirSnap)
-               const rejection = dir.sub(dirProjected)
-               if (rejection.sqLength() < sqSnapDistance) {
-                  segEnd = segStart.displaceBy(dirProjected)
-                  break
-               }
-            }
-         }
-         // Priority #4: Try snapping to segments.
-         if (!snappedToPoint) {
-            let snapDirection = segEnd.displacementFrom(segStart)
-            for (let [start, ends] of circuitDir) {
-               for (let end of ends) {
-                  snappedToPoint = trySnapToSegment(
-                     segEnd,
-                     new Segment(start, end),
-                     snapDirection
-                  )
-                  if (snappedToPoint) {
-                     segEnd = snappedToPoint
+      drawEnd = null
+      if (drawStart) {
+         // Try snapping to a point of significance.
+         drawEnd = trySnappingToPoint(mouse)
+         if (drawEnd === mouse) {
+            // If no snapping occurred, try snapping to an angle and/or a
+            // line segment instead.
+            const dir = mouse.displacementFrom(drawStart)
+            if (dir.sqLength() >= sqLengthAtWhichSnapsActivate) {
+               for (let dirSnap of snapVectors) {
+                  const dirProjected = dir.projectionOnto(dirSnap)
+                  const rejection = dir.sub(dirProjected)
+                  if (rejection.sqLength() < sqSnapDistance) {
+                     drawEnd = drawStart.displaceBy(dirProjected)
                      break
                   }
                }
             }
+            let snapDirection = drawEnd.displacementFrom(drawStart)
+            drawEnd = trySnappingToSegment(drawEnd, snapDirection)
          }
-      } else {
-         segEnd = null
       }
    }
 </script>
@@ -140,84 +139,56 @@
       mouse = new Point(event.clientX, event.clientY)
    }}
    on:pointerdown={(event) => {
-      segStart = new Point(event.clientX, event.clientY)
-      let snappedToPoint
-      // Priority #1: Try snapping to points along the reference segment.
-      if (reference) {
-         for (let p of reference.segment.points(reference.length - 1)) {
-            if (pointsCanSnap(segStart, p)) {
-               segStart = snappedToPoint = p
-               break
-            }
-         }
-      }
-      // Priority #2: Try snapping to segment endpoints.
-      if (!snappedToPoint) {
-         for (let p of circuitUndir.keys()) {
-            if (pointsCanSnap(segStart, p)) {
-               segStart = snappedToPoint = p
-               break
-            }
-         }
-      }
-      // Priority #3: Try snapping to segments.
-      if (!snappedToPoint) {
-         for (let [start, ends] of circuitDir) {
-            for (let end of ends) {
-               snappedToPoint = trySnapToSegment(
-                  segStart,
-                  new Segment(start, end)
-               )
-               if (snappedToPoint) {
-                  segStart = snappedToPoint
-                  break
-               }
-            }
-         }
+      let clickPosition = new Point(event.clientX, event.clientY)
+      // Try snapping to a point of significance.
+      drawStart = trySnappingToPoint(clickPosition)
+      if (drawStart === clickPosition) {
+         // If no snapping occurred, try snapping to a segment instead.
+         drawStart = trySnappingToSegment(drawStart)
       }
    }}
    on:pointerup={() => {
-      // Note: if segStart or segEnd have been chosen as the result of snapping,
-      // then they will refer to existing Point objects. This is crucial to
-      // ensuring that the circuit Maps and Sets accurately reflect the circuit
-      // topology.
-      if (segStart && segEnd) {
+      // Note: if drawStart or drawEnd have been chosen as the result of
+      // snapping, then they will refer to existing Point objects. This is
+      // crucial to ensuring that the circuit Maps and Sets accurately reflect
+      // the circuit topology.
+      if (drawStart && drawEnd) {
          // Update the directed circuit.
          // Don't add the segment "backwards" if it already appears forward.
-         if (!circuitDir.get(segEnd)?.has(segStart)) {
-            let segments = circuitDir.get(segStart)
+         if (!circuitDir.get(drawEnd)?.has(drawStart)) {
+            let segments = circuitDir.get(drawStart)
             if (segments) {
-               segments.add(segEnd)
+               segments.add(drawEnd)
             } else {
-               circuitDir.set(segStart, new Set([segEnd]))
+               circuitDir.set(drawStart, new Set([drawEnd]))
             }
          }
          circuitDir = circuitDir
          // Update the undirected circuit.
-         let segmentsAtStart = circuitUndir.get(segStart)
+         let segmentsAtStart = circuitUndir.get(drawStart)
          if (segmentsAtStart) {
-            segmentsAtStart.add(segEnd)
+            segmentsAtStart.add(drawEnd)
          } else {
-            circuitUndir.set(segStart, new Set([segEnd]))
+            circuitUndir.set(drawStart, new Set([drawEnd]))
          }
-         let segmentsAtEnd = circuitUndir.get(segEnd)
+         let segmentsAtEnd = circuitUndir.get(drawEnd)
          if (segmentsAtEnd) {
-            segmentsAtEnd.add(segStart)
+            segmentsAtEnd.add(drawStart)
          } else {
-            circuitUndir.set(segEnd, new Set([segStart]))
+            circuitUndir.set(drawEnd, new Set([drawStart]))
          }
          circuitUndir = circuitUndir
          // Reset the dragging start point
-         segStart = null
+         drawStart = null
       }
    }}
 >
-   <!-- {#if segStart && segEnd}
+   <!-- {#if drawStart && drawEnd}
       {#each segments as seg}
          <circle
             r={7}
-            cx={new Segment(segStart, segEnd).intersection(seg)?.x}
-            cy={new Segment(segStart, segEnd).intersection(seg)?.y}
+            cx={new Segment(drawStart, drawEnd).intersection(seg)?.x}
+            cy={new Segment(drawStart, drawEnd).intersection(seg)?.y}
          />
       {/each}
    {/if} -->
@@ -229,8 +200,8 @@
          {/each}
       {/each}
    {/each}
-   {#if segStart && segEnd}
-      <line x1={segStart.x} y1={segStart.y} x2={segEnd.x} y2={segEnd.y} />
+   {#if drawStart && drawEnd}
+      <line x1={drawStart.x} y1={drawStart.y} x2={drawEnd.x} y2={drawEnd.y} />
    {/if}
 </svg>
 
